382095415814051a00be322aa238fa955058ff68
Arifian Saputra <arifiansaputra43@gmail.com>
2021-06-12 21:52:04 +0700

    Update script-time.js

--- script-time.js
+++ script-time.js
@@ -53,5 +53,57 @@ function MonthstoAll() {
     inputNanosecs.value = Nanosecval;
 }
 
+function WeekstoAll() {
+    const Weeksval = parseFloat(inputWeeks.value);
+    const Yearsval = Weeksval * 0.02;
+    const Monthsval = Weeksval * 0.23;
+    const Daysval = Weeksval * 7;
+    const Hoursval = Weeksval * 168;
+    const Minutesval = Weeksval * 10080;
+    const Secsval = Weeksval * 604800.02;
+    const Msval = Weeksval * 604800016.56;
+    const Microsecval = Weeksval * 6.05e11;
+    const Nanosecval = Weeksval * 6.05e14;
+    inputYears.value = Yearsval;
+    inputMonths.value = Monthsval;
+    inputDays.value = Daysval;
+    inputHours.value = Hoursval;
+    inputMinutes.value = Minutesval;
+    inputSeconds.value = Secsval;
+    inputMilisecs.value = Msval;
+    inputMicrosecs.value = Microsecval;
+    inputNanosecs.value = Nanosecval;
+}
+
+function DaystoAll() {
+    const Daysval = parseFloat(inputDays.value);
+    const Yearsval = Daysval * 2.74e-3;
+    const Monthsval = Daysval * 0.03;
+    const Weeksval = Daysval * 0.14;
+    const Hoursval = Daysval * 24;
+    const Minutesval = Daysval * 1440;
+    const Secsval = Daysval * 86400;
+    const Msval = Daysval * 86400000;
+    const Microsecval = Daysval * 86400000000;
+    const Nanosecval = Daysval * 8.64e13;
+    inputYears.value = Yearsval;
+    inputMonths.value = Monthsval;
+    inputWeeks.value = Weeksval;
+    inputHours.value = Hoursval;
+    inputMinutes.value = Minutesval;
+    inputSeconds.value = Secsval;
+    inputMilisecs.value = Msval;
+    inputMicrosecs.value = Microsecval;
+    inputNanosecs.value = Nanosecval;
+}
+
 inputYears.addEventListener('input',YearstoAll);
 inputMonths.addEventListener('input',MonthstoAll);
+inputWeeks.addEventListener('input',WeekstoAll);
+inputDays.addEventListener('input',DaystoAll);
+inputHours.addEventListener('input',HourstoAll);
+inputMinutes.addEventListener('input',MinutestoAll);
+inputSeconds.addEventListener('input',SecondstoAll);
+inputMilisecs.addEventListener('input',MstoAll);
+inputMicrosecs.addEventListener('input',MicrosecstoAll);
+inputNanosecs.addEventListener('input',NanosecstoAll);
